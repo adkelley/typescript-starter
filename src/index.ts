6b4d3651c451af0d14955bbf32cdc9621733d7ed
Alex Kelley <adkelley@mac.com>
2019-01-04 10:32:47 -0800

    add: tslint rules

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
-import logIt from "./logit.ts";
+import logIt from "./logit.ts"
 
-function component() {
-    const element = document.createElement("div");
-    element.innerHTML = "Hello TypeScript";
+function component(): HTMLElement {
+    const element = document.createElement("div")
+    element.innerHTML = "Hello TypeScript"
 
-    logIt("I love types");
+    logIt("I love types")
 
-    return element;
+    return element
 }
 
-document.body.appendChild(component());
+(function main() {
+  document.body.appendChild(component())
+} ())
--- src/logit.ts
+++ src/logit.ts
@@ -1,3 +1,3 @@
 export default function logIt(text: string) {
-    console.log("I get called from logit.ts: ", text);
+    console.log('I get called from logit')  /* tslint:disable-line:no-console */
 }
